4d897f4792ca252fdf874e17daa262a214fffe44
kuncc <kuncc@foxmail.com>
2021-08-14 21:23:48 +0800

    实现主题切换

--- js/main.js
+++ js/main.js
@@ -92,12 +92,12 @@ const Setting = {
       this.$$contents.forEach($content => $content.classList.remove('active'))
       this.$$contents[index]?.classList.add('active')
     })
-  }
+  },
 }
 const Editor = {
   init() {
     let TPL = '# aweslide'
-    this.$saveBtn = $('.save')
+    this.$saveBtn = $('.saveMd')
     this.$editor = $('.content textarea')
     this.$sildes = $('.slides')
     this.$markdown = localStorage.markdown || TPL
@@ -122,9 +122,59 @@ const Editor = {
     })
   }
 }
+const Theme = {
+  init() {
+    this.themes = ['blood', 'beige', 'black', 'league', 'moon', 'night', 'serif', 'simple', 'sky', 'solarized', 'white']
+    this.currentTheme = localStorage.theme || 'blood'
+    this.currentIndex = this.themes.indexOf(this.currentTheme)
+    this.$theme = $('.theme')
+    this.$saveBtn = $('.saveTheme')
+    this.bind()
+    this.setTheme()
+    this.loadSwiper()
+  },
+  bind() {
+    this.$saveBtn.onclick = () => {
+      localStorage.theme = this.currentTheme
+      location.reload()
+    }
+  },
+  setTheme() {
+    const $link = document.createElement('link')
+    $link.rel = 'stylesheet'
+    $link.href = `css/theme/${this.currentTheme}.css`
+    document.head.appendChild($link)
+  },
+  loadSwiper() {
+    const {currentIndex} = this
+    const swiper = new Swiper('.mySwiper', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 4,
+        slideShadows: false,
+      },
+      on: {
+        init() {
+          this.activeIndex = currentIndex
+        },
+      },
+    })
+    swiper.on('slideChange', e => {
+      this.currentTheme = this.themes[e.activeIndex]
+      this.$theme.innerText = this.currentTheme
+    })
+    this.$theme.innerText = this.currentTheme
+  }
+}
 const App = {
   init() {
     [...arguments].forEach(module => module.init())
   }
 }
-App.init(Setting, Editor)
+App.init(Setting, Editor, Theme)
